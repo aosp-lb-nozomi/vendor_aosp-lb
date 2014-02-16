ea0bbd9bde7f39aa795bebdb6f068e62a13c351d
djolivier <olivieer@users.noreply.github.com>
2014-02-16 23:19:22 +0100

    Create aoba.mk

---
+++ products/aoba.mk
@@ -0,0 +1,21 @@
+$(call inherit-product, device/sony/aoba/full_aoba.mk)
+
+# Boot Animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
+# Inherit common device configuration for AOSP
+$(call inherit-product, vendor/aosp-lb/common_aosp.mk)
+
+# Add all AOSP ringtones(disable for more space on /system)
+$(call inherit-product, frameworks/base/data/sounds/AllAudio.mk)
+
+
+# Setup device specific product configuration.
+PRODUCT_NAME := aosp_aoba
+PRODUCT_BRAND := Sony
+PRODUCT_DEVICE := aoba
+PRODUCT_MODEL := Xperia ion
+PRODUCT_MANUFACTURER := Sony
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=LT28h BUILD_FINGERPRINT=SEMC/LT28h_1265-2665/LT28h:4.1.2/6.2.B.0.211/LL__zg:user/release-keys PRIVATE_BUILD_DESC="LT28h-user 4.1.2 6.2.B.0.211 LL__zg test-keys"
