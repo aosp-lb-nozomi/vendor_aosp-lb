2add8ff3b4fd9f04dc0aaa8420e8398092235692
djolivier <olivieer@users.noreply.github.com>
2014-02-16 23:12:07 +0100

    Update nozomi.mk

--- products/nozomi.mk
+++ products/nozomi.mk
@@ -4,22 +4,19 @@ $(call inherit-product, device/sony/nozomi/full_nozomi.mk)
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
-# Inherit common device configuration for PolishBlood Project 
-$(call inherit-product, vendor/PolishBlood/common_PolishBlood.mk)
+# Inherit common device configuration for AOSP
+$(call inherit-product, vendor/aosp-lb/common_aosp.mk)
 
 # Add all AOSP ringtones(disable for more space on /system)
 $(call inherit-product, frameworks/base/data/sounds/AllAudio.mk)
 
 
 # Setup device specific product configuration.
-PRODUCT_NAME := PolishBlood_nozomi
+PRODUCT_NAME := full_nozomi
 PRODUCT_BRAND := Sony
 PRODUCT_DEVICE := nozomi
 PRODUCT_MODEL := Xperia S
 PRODUCT_MANUFACTURER := Sony
-
-#skip asserts for now
-TARGET_OTA_ASSERT_SKIP := true
 
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=LT26i BUILD_FINGERPRINT=SEMC/LT26i_1257-6919/LT26i:4.1.2/6.2.B.0.200/N7__zg:user/release-keys PRIVATE_BUILD_DESC="LT26i-user 4.1.2 6.2.B.0.200 N7__zg test-keys"
